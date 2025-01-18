9ab8e1a88a707a5aa24ba7f7cf57e9b30753ebc3
ikeda-Java2307 <2201100202qv@cyber-u.ac.jp>
2025-01-18 12:58:30 +0900

    add env

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-
+require('dotenv').config();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
@@ -14,7 +14,7 @@ const database = client.db('notes');
 const notes = database.collection('notes');
 
 // idが１のドキュメントを取得
-const query = { id: 2 };
+const query = { id: 1 };
 const note = await notes.findOne(query);
 
 res.json(note);
